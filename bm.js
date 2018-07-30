fdb423c28c1dffeb9bf73050cb2fddd55bb441f3
devmer2 <devon.merlette@yale.edu>
2018-07-30 13:47:11 -0400

    updated files

--- bm.js
+++ bm.js
@@ -1,14 +1,49 @@
-$('head').append('<link rel="stylesheet" href="https://github.com/devmer2/interactive/blob/master/bm.css" type="text/css" />');
+(function(){
+// This part checks for jQuery
+var version = "1.10.2";
+// Checks for prior inclusion and version
+if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
+        // If there isn't an instance of jQuery, create one and append it to the head
+        // Else run our bookmarklet!
+        var done = false;
+        var script = document.createElement("script");
+        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + version + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function(){
+                if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                        done = true;
+                        initBookmarklet(window.jQuery);
+                }
+    else {
+        console.log('error loading');
+    }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
+    console.log(':::');
+}
+else {
+    console.log('jquery loaded');
+}
+} else {
+        initBookmarklet(window.jQuery);
+}
+function initBookmarklet($) {
+        (window.bookmarklet = function() {
 
-// adds span around chars in p tag
-$('p').each(function() {
+          $('head').append('<link rel="stylesheet" href="https://github.com/devmer2/interactive/blob/master/bm.css" type="text/css" />');
 
-  var string = $(this).html();
-  var newString = "";
+          // adds span around chars in p tag
+          $('p').each(function() {
 
-  for (var i = 0; i < string.length; i++) {
-    newString += '<span class="char">' + string[i] + '</span>';
-  }
+            var string = $(this).html();
+            var newString = "";
 
-  $(this).html(newString); // replaces with newString
-});
+            for (var i = 0; i < string.length; i++) {
+              newString += '<span class="char">' + string[i] + '</span>';
+            }
+
+            $(this).html(newString); // replaces with newString
+          });
+        })();
+}
+})();
