ff71ce15dd0a878485ca6aad3c74832511b7afe8
devmer2 <devon.merlette@yale.edu>
2018-07-30 13:36:58 -0400

    updated files

--- bm.js
+++ bm.js
@@ -1,4 +1,4 @@
-$('head').append('<link rel="stylesheet" href="bm.css" type="text/css" />');
+$('head').append('<link rel="stylesheet" href="https://github.com/devmer2/interactive/blob/master/bm.css" type="text/css" />');
 
 // adds span around chars in p tag
 $('p').each(function() {
